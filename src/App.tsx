0826d9d2b0282c5a8949bf09df1673ee97192d59
ivanreeve <ivanreevelopez@gmail.com>
2024-09-09 18:02:12 +0800

    add buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import "react/jsx-runtime"
-import "./index.css"
 import { Button } from "@/components/ui/button"
 
 function ArrowRightIcon(props: any) {
@@ -7,8 +5,8 @@ function ArrowRightIcon(props: any) {
     <svg
       {...props}
       xmlns="http://www.w3.org/2000/svg"
-      width="100"
-      height="100"
+      width="24"
+      height="24"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
@@ -22,21 +20,39 @@ function ArrowRightIcon(props: any) {
   )
 }
 
-function OverlayButton() {
+function OverlayButton({ name }: { name: string }, { iconPath }: { iconPath: string }) {
   return (
-    <Button className="w-[400px] h-[400px] flex flex-col active:bg-gray-200 hover:bg-white hover:text-black transform active:scale-90 select-none outline-none shadow-lg hover:shadow-xl transition-shadow duration-150 ease-in transition-all duration-150 ease-in-out">
-      <ArrowRightIcon className="h-600 w-600" />
-      <p className="text-4xl">Overlay</p>
+    <Button className="
+      w-[400px]
+      h-[400px]
+      flex
+      flex-col
+      justify-evenly
+      active:bg-gray-200
+      hover:bg-white
+      hover:text-black
+      transform active:scale-90
+      select-none
+      outline-none
+      shadow-lg
+      hover:shadow-xl
+      transition-all
+      duration-150
+      ease-in-out
+      ">
+      <Icon path={ iconPath } />
+      <p className="text-4xl">{ name }</p>
     </Button>
   )
 }
 
 export default function App() {
   return (
-    <div className="flex flex-row space-x-20 justify-center items-center">
-      <OverlayButton />
-      <OverlayButton />
-      <OverlayButton />
+    <div className="h-screen w-screen flex flex-row justify-center items-center space-x-20">
+        <OverlayButton name="Overlay" />
+        <OverlayButton name="Controller" />
+        <OverlayButton name="In-game" />
     </div>
   )
 }
+
