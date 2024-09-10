8bda1222838d2728ede03f51d1186c2101f47021
ivanreeve <ivanreevelopez@gmail.com>
2024-09-10 21:10:19 +0800

    todo: button icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import { Button } from "@/components/ui/button"
 
-function ArrowRightIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M5 12h14" />
-      <path d="m12 5 7 7-7 7" />
-    </svg>
-  )
-}
+// function ArrowRightIcon(props: any) {
+//   return (
+//     <svg
+//       {...props}
+//       xmlns="http://www.w3.org/2000/svg"
+//       width="24"
+//       height="24"
+//       viewBox="0 0 24 24"
+//       fill="none"
+//       stroke="currentColor"
+//       strokeWidth="2"
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//     >
+//       <path d="M5 12h14" />
+//       <path d="m12 5 7 7-7 7" />
+//     </svg>
+//   )
+// }
 
 function OverlayButton({ name }: { name: string }, { iconPath }: { iconPath: string }) {
   return (
